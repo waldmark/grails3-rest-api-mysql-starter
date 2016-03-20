d251194c0dc73b640f96bd93b4f959bf9597cac3
Mark Wald <mark.wald@objectpartners.com>
2016-03-20 15:35:08 -0500

    added configuration stuff to BootStrap.groovy

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,13 +1,36 @@
 import com.idx.oem.chrysler.TestDomain
+import com.idx.oem.chrysler.VinService
+import grails.core.GrailsApplication
+import org.grails.io.support.ClassPathResource
+
+import javax.servlet.ServletContext
 
 class BootStrap {
 
-    def vinService
+    GrailsApplication grailsApplication
+    VinService vinService
 
-    def init = { servletContext ->
+    def init = { ServletContext ctx ->
+
+        // clear out old data
+        TestDomain.executeUpdate("delete from TestDomain")
+
+        def dbUrl = grailsApplication.config.getProperty('dataSource.url')
+        println "database url = ${dbUrl}"
+
+        def file1 = new ClassPathResource('public/data.txt').file
+        println "file = ${file1.absolutePath}"
+
+        String fileContents = file1.text
+        def tokens = fileContents.split(",")
+
+        tokens.each { token ->
+            println token
+        }
+
         def tests = []
-        tests.add(new TestDomain(vin: "12345"))
-        tests.add(new TestDomain(vin: "67890"))
+        tests.add(new TestDomain(vin: "T4-12345"))
+        tests.add(new TestDomain(vin: "T4-67890"))
 
         tests.each{ vin ->
             if(vinService.validate(vin)) {
@@ -16,9 +39,10 @@ class BootStrap {
         }
 
         def saved = TestDomain.findAll()
-        saved.each { test ->
+        saved.each { TestDomain test ->
             println "saved: ${test.vin}"
         }
+
     }
 
     def destroy = {
